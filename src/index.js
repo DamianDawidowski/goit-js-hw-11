79f08c548e260f3f35f0434023da17815c2ceeb8
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-28 22:40:20 +0100

    deleted:    README.en.md 	modified:   README.md 	new file:   README.ru.md 	modified:   package-lock.json 	modified:   package.json 	modified:   src/css/styles.css 	modified:   src/index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,74 +1,127 @@
+import axios from 'axios';
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
-import debounce from 'lodash.debounce';
+
+// import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
-
+const form = document.querySelector('form');
 const input = document.querySelector('input');
-const countryList = document.querySelector('.country-list');
-const countryDetails = document.querySelector('.country-info');
+const searchBtn = document.querySelector('button');
+const imageGallery = document.querySelector('.gallery');
 
-input.addEventListener(
-  'input',
-  debounce(() => {
-    if (!input.value.trim()) {
-      countryList.textContent = '';
-      countryDetails.textContent = '';
-      return;
-    }
 
-    fetchCountries(`${input.value.trim()}`)
-      .then(countries => {
-        if (countries.length > 10) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
-          return;
-        } else if (countries.length > 2) {
-          rendercountryList(countries);
-        } else if (countries.length == 1) {
-          renderSingle(countries);
-        }
-      })
-      .catch(error => {
-        Notiflix.Notify.failure(`Oops, there is no country with that name`);
-      });
-  }, DEBOUNCE_DELAY)
-);
+// var axios = require('axios');
 
-function rendercountryList(countries) {
-  const markup = countries
-    .map(country => {
-      return `<li class="eachCountry"><img class="eachFlag" width =20 height=15 src=${country.flags.svg} />
-      <p class="eachName">${country.name}</p>
-      </li>
-      `;
+  
+
+form.addEventListener('submit', (e) => {
+  console.log(input.value);
+  e.preventDefault();
+  if (!input.value.trim()) {
+    // images.textContent = '';
+    console.log('WORKS2');
+    // Notiflix.Notify.failure(`Oops, there is no country with that name`);
+    return;
+  }
+
+  fetchImages(`${input.value.trim()}`)
+    .then(countries => {
+      console.log('WORKS3');
+      console.log(input.value);
+
+      renderImages(countries);
     })
+    .catch(error => {
+      Notiflix.Notify.failure(`Oops, there is no country with that name`);
+      console.log('WORKS4');
+    });
+});
+
+function renderImages(images) {
+  if (images.hits.length == 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  const markup = images.hits
+     
+    .map(image => {
+       console.log('WORKS6');
+      return `<div class="photo-card">
+        <img src=${image.webformatURL} alt=${image.tags} loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${image.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${image.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${image.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${image.downloads}
+          </p>
+        </div>
+      </div>`;
+
+  
+      //  (extraHTML = <div class="eachimage"><img class="eachFlag"  src=${image.webformatURL} /> </div>)
+      
+    //    <h2 class="singleimageName">${image.name}</h2>
+    //    </div>
+    //  <div class="eachInfo"> <span class="eachHighlight">Capital: </span><p class="eachValue"> ${image.capital}</p></div>
+    //   <div class="eachInfo"> <span class="eachHighlight">Population: </span><p class="eachValue"> ${image.population}</p></div>
+    //    <div class="eachInfo"> <p class="eachHighlight removeMargin">Languages: </p><span class="eachValue">${image.languages[0].name};
+
+      // for (let i = 1; i < image.languages.length - 1; i++) {
+      //   extraHTML = extraHTML + `, ${image.languages[i].name};
+      // }
+
+      // extraHTML = extraHTML + ` </span></div>`;
+  
+    })
+
     .join('');
-  countryList.innerHTML = markup;
-  countryDetails.innerHTML = '';
+   console.log(images);
+  imageGallery.innerHTML = markup;
 }
 
-function renderSingle(countries) {
-  const markup = countries
-    .map(country => {
-      let extraHTML = `<div class="eachCountry"><img class="eachFlag" width =22 height=16 src=${country.flags.svg} />
-      <h2 class="singleCountryName">${country.name}</h2>
-      </div>
-    <div class="eachInfo"> <span class="eachHighlight">Capital: </span><p class="eachValue"> ${country.capital}</p></div>
-     <div class="eachInfo"> <span class="eachHighlight">Population: </span><p class="eachValue"> ${country.population}</p></div>
-      <div class="eachInfo"> <p class="eachHighlight removeMargin">Languages: </p><span class="eachValue">${country.languages[0].name}`;
-
-      for (let i = 1; i < country.languages.length - 1; i++) {
-        extraHTML = extraHTML + `, ${country.languages[i].name}`;
-      }
-
-      extraHTML = extraHTML + ` </span></div>`;
-      return extraHTML;
-    })
-
-    .join('');
-  countryDetails.innerHTML = markup;
-  countryList.innerHTML = '';
+async function fetchImages(query) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=34020653-837b1231ff9ac2e46753275a8&q=${query}&image_type=photo&orientation=hohorizontal&safesearch=true`
+    );
+    console.log(`Pixabay response is ` + response);
+    console.log( response.hits);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+
+
+
+  // function fetchImages(query) {
+  // return   axios
+
+
+// async function fetchImages(query) {
+//   return await axios
+//     .get(
+//       `https://pixabay.com/api/?key=34020653-837b1231ff9ac2e46753275a8&q=${query}&image_type=photo`
+//     )
+//     .then(response => {
+//       if (!response.ok) {
+//         console.log('WORKS7');
+//         throw new Error(response.status);
+//       }
+
+//       return response.json();
+//     });
+// }
+
+//XXXXXXXXXXXXXXXX
+// 34020653-837b1231ff9ac2e46753275a8
