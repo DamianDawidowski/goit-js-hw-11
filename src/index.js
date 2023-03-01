b01241f97e0da0369ad6343e6853040ecd73cb59
Damian Dawidowski <damiandawidowski@gmail.com>
2023-03-01 22:19:18 +0100

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -6,60 +6,32 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('form');
 const input = document.querySelector('input');
-const searchBtn = document.querySelector('button');
 const imageGallery = document.querySelector('.gallery');
 let page = 0;
-//  let lightbox = new SimpleLightbox('.gallery a', {
-//    captionPosition: 'bottom',
-//    captionSelector: 'img',
-//    captionsData: 'alt',
-//    captionType: 'attr',
-//    captionDelay: 250,
-//  });
-
-// const lightbox = () => new SimpleLightbox('.gallery a', {
-//    captionPosition: 'bottom',
-//      captionSelector: 'img',
-//    captionsData: 'alt',
-//      captionType: 'attr',
-//     captionDelay: 250,
-//    });
-
-
-// var axios = require('axios');
 
 form.addEventListener('submit', e => {
   page = 1;
-  console.log(input.value);
   e.preventDefault();
   if (!input.value.trim()) {
-    // images.textContent = '';
-    console.log('WORKS2');
-    // Notiflix.Notify.failure(`Oops, there is no country with that name`);
     return;
   }
 
   imageGallery.textContent = '';
   fetchImages(`${input.value.trim()}`)
     .then(images => {
-      console.log('WORKS3');
-      console.log(input.value);
-new SimpleLightbox('.gallery a');
+      new SimpleLightbox('.gallery a');
       renderImages(images);
-const { height: cardHeight } = document
-  .querySelector('.gallery')
-  .firstElementChild.getBoundingClientRect();
-// lightbox().refresh();
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: 'smooth',
-});
-
- 
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+      lightbox().refresh();
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
     })
     .catch(error => {
       Notiflix.Notify.failure(`Oops, there is no country with that name`);
-      console.log('WORKS4');
     });
 });
 
@@ -72,9 +44,7 @@ function renderImages(images) {
   }
   Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
   const markup = images.hits
-
     .map(image => {
-      console.log('WORKS6');
       return `<div class="photo-card">
        <a class="gallery__item" href="${image.largeImageURL}"> <img src=${image.webformatURL} alt=${image.tags} loading="lazy" />
         <div class="info">
@@ -93,9 +63,7 @@ function renderImages(images) {
         </div>
       </div>`;
     })
-
     .join('');
-  console.log(images);
   imageGallery.insertAdjacentHTML('beforeend', markup);
 }
 
@@ -104,33 +72,24 @@ async function fetchImages(query) {
     const response = await axios.get(
       `https://pixabay.com/api/?key=34020653-837b1231ff9ac2e46753275a8&q=${query}&image_type=photo&orientation=hohorizontal&safesearch=true&page=${page}&per_page=40`
     );
-    console.log(`Pixabay response is ` + response);
-    console.log(response.hits);
     return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
 }
 
 window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-  setTimeout(console.log({ scrollTop, scrollHeight, clientHeight }), 1000);
-
   if (clientHeight + scrollTop >= scrollHeight - 5) {
     page++;
-    // show the loading animation
+    if (page >= totalPages) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return;
+    }
     fetchImages(`${input.value.trim()}`).then(images => {
-      console.log('WORKS3');
-      console.log(input.value);
-      console.log(`after scrolling current page value is ${page}`);
       renderImages(images);
-       lightbox().refresh();;
+      lightbox().refresh();
       new SimpleLightbox('.gallery a');
     });
-    setTimeout(console.log('LOAD'), 1000);
   }
 });
-
-//XXXXXXXXXXXXXXXX
-// 34020653-837b1231ff9ac2e46753275a8
